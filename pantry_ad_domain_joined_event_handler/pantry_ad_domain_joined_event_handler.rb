2461fbe87b6870fe24164cf3a169fdd9f6fc27a6
John Coleman <john.coleman@wonga.com>
2013-08-23 14:51:58 +0100

    Update log messages

--- pantry_ad_domain_joined_event_handler/pantry_ad_domain_joined_event_handler.rb
+++ pantry_ad_domain_joined_event_handler/pantry_ad_domain_joined_event_handler.rb
@@ -7,9 +7,9 @@ module Wonga
       end
 
       def handle_message(message)
-        @logger.info "Updating booted status for Request:#{message['pantry_request_id']}, Name:#{message['instance_name']}, InstanceID:#{message['instance_id']}"
+        @logger.info "Updating domain joined status for Request:#{message['pantry_request_id']}, Name:#{message['instance_name']}, InstanceID:#{message['instance_id']}"
         @api_client.update_ec2_instance(message['pantry_request_id'], { joined: true, instance_id: message['instance_id'] })
-        @logger.info "Updating booted status for Request:#{message['pantry_request_id']} succeeded"
+        @logger.info "Updating domain joined status for Request:#{message['pantry_request_id']} succeeded"
       end
     end
   end
